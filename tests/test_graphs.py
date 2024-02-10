3fa5c90f843aec4a9004470d14a1cecddc5ecbf3
0razzu <0razzu@protonmail.com>
2024-02-10 17:55:20 +0300

    graphs: more tests

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -3,13 +3,34 @@ import pytest
 from textwrap import dedent
 
 
-def test_get_props_by_name():
+def test_get_props_by_name__ls():
+    assert graphs.GraphProps(1687, 1453, {"a", "d"}) == graphs.get_props_by_name("ls")
+
+
+def test_get_props_by_name__univ():
     assert graphs.GraphProps(
-        node_quan=1687, edge_quan=1453, labels={"a", "d"}
-    ) == graphs.get_props_by_name("ls")
+        node_quan=179,
+        edge_quan=293,
+        labels={
+            "type",
+            "label",
+            "subClassOf",
+            "domain",
+            "range",
+            "first",
+            "rest",
+            "someValuesFrom",
+            "onProperty",
+            "intersectionOf",
+            "subPropertyOf",
+            "inverseOf",
+            "versionInfo",
+            "comment",
+        },
+    ) == graphs.get_props_by_name("univ")
 
 
-def test_save_labeled_two_cycle_graph(tmp_path):
+def test_save_labeled_two_cycle_graph__c_2_5(tmp_path):
     graph_path = tmp_path / "c2,5.dot"
 
     graphs.save_labeled_two_cycle_graph(
@@ -33,6 +54,37 @@ def test_save_labeled_two_cycle_graph(tmp_path):
             3 -> 4  [key=0, label=b];
             4 -> 5  [key=0, label=b];
             5 -> 0  [key=0, label=b];
+            }
+            """
+        )
+
+        assert expected == "".join(graph_file.readlines())
+
+
+def test_save_labeled_two_cycle_graph__c_4_4(tmp_path):
+    graph_path = tmp_path / "c4,4.dot"
+
+    graphs.save_labeled_two_cycle_graph(3, 3, ("0", "123"), graph_path)
+
+    with open(graph_path, "r") as graph_file:
+        expected = dedent(
+            """\
+            digraph  {
+            1;
+            2;
+            3;
+            0;
+            4;
+            5;
+            6;
+            1 -> 2  [key=0, label=0];
+            2 -> 3  [key=0, label=0];
+            3 -> 0  [key=0, label=0];
+            0 -> 1  [key=0, label=0];
+            0 -> 4  [key=0, label=123];
+            4 -> 5  [key=0, label=123];
+            5 -> 6  [key=0, label=123];
+            6 -> 0  [key=0, label=123];
             }
             """
         )
