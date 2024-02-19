2aa52b8246981d62b079e9b167a31c95e169cdc2
0razzu <0razzu@protonmail.com>
2024-02-20 00:43:29 +0300

    finite_automatons: failing graph_to_nfa()

--- project/finite_automatons.py
+++ project/finite_automatons.py
@@ -1,6 +1,10 @@
 from pyformlang.regular_expression import Regex
-from pyformlang.finite_automaton import DeterministicFiniteAutomaton
-from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    NondeterministicFiniteAutomaton,
+    State,
+    Symbol,
+)
 from networkx import MultiDiGraph
 from typing import Set
 
@@ -10,5 +14,25 @@ def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
 
 
 def graph_to_nfa(
-    graph: MultiDiGraph, start_states: Set[int], final_states: Set[int]
-) -> NondeterministicFiniteAutomaton: ...
+    graph: MultiDiGraph,
+    start_nodes: Set[int],
+    final_nodes: Set[int],
+) -> NondeterministicFiniteAutomaton:
+    if start_nodes is None:
+        start_nodes = graph.nodes()
+
+    if final_nodes is None:
+        final_nodes = graph.nodes()
+
+    nfa = NondeterministicFiniteAutomaton()
+
+    for start_node in start_nodes:
+        nfa.add_start_state(State(start_node))
+
+    for final_node in final_nodes:
+        nfa.add_final_state(State(final_node))
+
+    for from_node, to_node, label in graph.edges(data="label"):
+        nfa.add_transition(State(from_node), Symbol(label), State(to_node))
+
+    return nfa
