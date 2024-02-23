5ced517b2fb453575133a3bdd30706d30d1c4257
0razzu <0razzu@protonmail.com>
2024-02-23 19:31:21 +0300

    finite_automatons: simplified graph_to_nfa()

--- project/finite_automatons.py
+++ project/finite_automatons.py
@@ -4,8 +4,6 @@ from pyformlang.finite_automaton import (
     NondeterministicFiniteAutomaton,
     State,
     Symbol,
-    Epsilon,
-    EpsilonNFA,
 )
 from networkx import MultiDiGraph
 from typing import Set
@@ -20,20 +18,10 @@ def graph_to_nfa(
     start_nodes: Set[int],
     final_nodes: Set[int],
 ) -> NondeterministicFiniteAutomaton:
-    if start_nodes is None or len(start_nodes) == 0:
-        start_nodes = graph.nodes()
+    start_nodes = start_nodes or set(graph.nodes())
+    final_nodes = final_nodes or set(graph.nodes())
 
-    if final_nodes is None or len(final_nodes) == 0:
-        final_nodes = graph.nodes()
-
-    start_nodes = set(start_nodes)
-    final_nodes = set(final_nodes)
-
-    for node in graph.nodes(data=True):
-        node[1].update({"is_start": node[0] in start_nodes})
-        node[1].update({"is_final": node[0] in final_nodes})
-
-    nfa = EpsilonNFA()
+    nfa = NondeterministicFiniteAutomaton()
 
     for start_node in start_nodes:
         nfa.add_start_state(State(start_node))
@@ -41,14 +29,7 @@ def graph_to_nfa(
     for final_node in final_nodes:
         nfa.add_final_state(State(final_node))
 
-    print(start_nodes)
-    print(final_nodes)
     for from_node, to_node, label in graph.edges(data="label"):
-        print(from_node, to_node, label)
-        nfa.add_transition(
-            State(from_node),
-            Epsilon() if label == "ɛ" else Symbol(label),
-            State(to_node),
-        )
+        nfa.add_transition(State(from_node), Symbol(label), State(to_node))
 
     return nfa
